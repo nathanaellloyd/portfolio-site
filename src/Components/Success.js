efacac15c81d5adf5d331ff9938e553dc64189f9
natelloyd1 <nathanaellloyd@live.com>
2020-07-11 17:54:06 +0100

    add success page and styling

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,5 +1,26 @@
 import React from 'react'; 
 
-export default function Success() {
-    return <div>Form successfully submitted!</div>
-  }
+function Success() {
+
+
+  const Emoji = props => (
+    <span
+      className="emoji"
+      role="img"
+    >
+      {props.symbol}
+    </span>
+  )
+
+
+    return (
+        <>  
+            <div className="success-square">
+            <p className="success-message">Thank you for your message, you'll hear back from me soon <Emoji symbol="✌🏻"/></p>
+            </div>  
+        </>
+    )
+
+}
+
+export default Success; 
